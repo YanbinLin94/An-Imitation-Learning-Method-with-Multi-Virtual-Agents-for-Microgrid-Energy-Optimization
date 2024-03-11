f4bdb830cd59ef46eb2f73697551222c9c0c808d
YanbinLin94 <97860537+YanbinLin94@users.noreply.github.com>
2024-03-11 10:48:27 -0400

    Update main.py

--- MAIL_code/GM2024_Python/main.py
+++ MAIL_code/GM2024_Python/main.py
@@ -1,7 +1,9 @@
-%%%%%%%%% This code is written by Yanbin Lin in 2023.%%%%%%%%%
-% If this code is used for any research purpose, please cite our PESGM’24 paper below.
-% Yanbin Lin, Zhen Ni, and Yufei Tang, “An Imitation Learning Method with Multi virtual Agents for Microgrid Energy Optimization under Interrupted Periods,” in Proc. of IEEE Power & Energy Society General Meeting (PESGM’24), pp.1-5, Washington, DC, USA, Jul. 21-25, 2024. 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+"""
+Copyright 2024 Yanbin Lin. All rights reserved.
+This code is written by Yanbin Lin in 2023.
+If this code is used for any research purpose, please cite our PESGM’24 paper below.
+Yanbin Lin, Zhen Ni, and Yufei Tang, “An Imitation Learning Method with Multi virtual Agents for Microgrid Energy Optimization under Interrupted Periods,” in Proc. of IEEE Power & Energy Society General Meeting (PESGM’24), pp.1-5, Washington, DC, USA, Jul. 21-25, 2024. 
+"""
 
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
