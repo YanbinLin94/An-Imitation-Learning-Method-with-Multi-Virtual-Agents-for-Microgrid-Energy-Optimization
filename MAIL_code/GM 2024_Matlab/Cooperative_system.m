6f9137dc152b397edde9b35102507a40ba65a429
YanbinLin94 <97860537+YanbinLin94@users.noreply.github.com>
2024-03-11 13:08:03 -0400

    Update Cooperative_system.m

--- MAIL_code/GM 2024_Matlab/Cooperative_system.m
+++ MAIL_code/GM 2024_Matlab/Cooperative_system.m
@@ -19,7 +19,7 @@ for tab_time = 1:K
         value_table{tab_stat,tab_time} = Each_cell;     
     end
 end
-%cost_table = zeros(K,size(states,2));
+
 states_power = states*Es;
 initial_pos = 6;
 gamma = 1;
@@ -32,14 +32,9 @@ epsilon = 0.5;
 N_repeat = 1;
 help_ep = 100;
 
-%for repeat = 1:N_repeat
-    %value_table = zeros(size(states,2),K);
 lambda = -lambda;
 event_prob = 0;
 fault_time = [10 11 12 13 14 15];
-% lambda_fault = lambda1;
-% lambda_fault(fault_time) = 1000;
-% lambda = lambda_fault;
 
 for rr = 1:Run 
     for tab_time = 1:K
@@ -50,12 +45,10 @@ for rr = 1:Run
     epsilon = 0.5;
     help_ep = 50;
     tic
-for i = 1:iter 
-    
-    
+for i = 1:iter     
     initial_pow = L0*Es;
     stateK0 = [L0 L0];
-    %alpha = a/(a+i-1);
+
     alpha = 0.05;
     if i == help_ep
        epsilon =  epsilon/1.1;
@@ -77,28 +70,12 @@ for i = 1:iter
         status(index_inf) = 0;
         end
         help_assign = find(status>0);
-        %value_determin = value_table(:,time); 
+
         
         if rand < epsilon 
-   
-%         if time == fault_time(1) || time == fault_time(2) || time == fault_time(3) || time == fault_time(4)
-%             assign_flt = find(power(help_assign,2)==0);
-%             help_assign = help_assign(assign_flt);
-%             taking_action = help_assign(randi([1,size(help_assign,2)],1,1));
-%             h_pos = find(help_assign == taking_action);
-%             %cost_cal = [];
-%             for tw_st = 1:2
-%                 real_batt = power(help_assign(h_pos),tw_st);
-%                 [cost_cal,val_out] = F_costval_RL(real_batt,time,DA_load,DA_E,DA_price);
-%                 cost_agts(tw_st) = cost_cal;
-%             end
-%             %cost_cal = (event_prob*(power(help_assign(h_pos),1)*lambda(time)))+((1-event_prob)*(power(help_assign(h_pos),2)*lambda(time))); 
-%             optimized_cost = sum(cost_agts.*[event_prob (1-event_prob)]);
-%         else
             taking_action = help_assign(randi([1,size(help_assign,2)],1,1));
             h_pos = find(help_assign == taking_action);
-%             cost_cal = (0.5*(power(help_assign(h_pos),1)*lambda(time)))+(0.5*(power(help_assign(h_pos),2)*lambda(time))); 
-%             optimized_cost = cost_cal;
+
             for tw_st = 1:2
                 if tw_st == 1
                 real_batt = power(help_assign(h_pos),tw_st);
@@ -109,30 +86,14 @@ for i = 1:iter
                 [cost_cal,val_out] = F_costval_RL_FLT(real_batt,time,DA_load,DA_E,DA_price);
                 cost_agts(tw_st) = cost_cal;
                 end
-            end
-            %cost_cal = (event_prob*(power(help_assign(h_pos),1)*lambda(time)))+((1-event_prob)*(power(help_assign(h_pos),2)*lambda(time))); 
+            end            
             optimized_cost = sum(cost_agts.*[event_prob (1-event_prob)]);
-        %end
         
         else
         if time < K
-%         if time == fault_time(1) || time == fault_time(2) || time == fault_time(3) || time == fault_time(4)
-%             assign_flt = find(power(help_assign,2)==0);
-%             help_assign = help_assign(assign_flt);
-%             cost_cal = value_table{save_transition,time}(1,help_assign);
-%             [opt_cost,pos] = min(cost_cal);
-%             taking_action = help_assign(pos);
-%             for tw_st = 1:2
-%                 real_batt = power(taking_action,tw_st);
-%                 [cost_cal,val_out] = F_costval_RL(real_batt,time,DA_load,DA_E,DA_price);
-%                 cost_agts(tw_st) = cost_cal;
-%             end
-%             optimized_cost = sum(cost_agts.*[event_prob (1-event_prob)]); 
-%         else
             cost_cal = value_table{save_transition,time}(1,help_assign);
             [opt_cost,pos] = min(cost_cal);
             taking_action = help_assign(pos);
-            %optimized_cost = (0.5*(power(taking_action,1)*lambda(time)))+(0.5*(power(taking_action,2)*lambda(time))); 
             for tw_st = 1:2
                 if tw_st == 1
                 real_batt = power(taking_action,tw_st);
@@ -146,7 +107,6 @@ for i = 1:iter
               
             end
             optimized_cost = sum(cost_agts.*[event_prob (1-event_prob)]); 
-        %end
         
         else
         for all_poss = 1:size(help_assign,2)
@@ -165,35 +125,19 @@ for i = 1:iter
             cost_cal(all_poss) = sum(cost_agts.*[event_prob (1-event_prob)]); 
             save_mic_pol(all_poss,:) = val_out;
         end
-        %cost_cal = (0.5*(power(help_assign,1)*lambda(time)))+(0.5*(power(help_assign,2)*lambda(time))); 
         [optimized_cost,pos] = min(cost_cal);
         taking_action = help_assign(pos);  
         val_out = save_mic_pol(pos,:);
         end   
 
         end
-        %if time < K
-        %value_determin = value_table(:,time);
-%         else 
-%         value_determin = zeros(17,1);
-%         end
-        
-%         cost_cal = power(help_assign)*lambda(time) + value_determin(help_assign)';
-%         [optimized_cost,pos] = min(cost_cal);
         pos_soc = combine_st(taking_action,:);
         
         if time < K-1 
-           %[max_q_val] = ns_q(pos_soc,states,time,value_table,eta_ch,eta_dis,pmax_dis,pmax_ch,Es,lambda); 
            max_q_val = min(value_table{taking_action,time+1});
-        end
-        
-        
-        
+        end        
         
         if time < K-1
-%            if time == 1 
-%               save_transition = [6 taking_action]; 
-%            end
            value_table{save_transition,time}(1,taking_action) = optimized_cost + max_q_val; 
         else if time == K-1
            value_table{save_transition,time}(1,taking_action) = optimized_cost;
